28524dff285096f1f7b8f31833baddd28976c2d3
profxaker <profxaker@mail.ru>
2014-11-02 19:28:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,86 @@
 <?php
-  echo 'Hello World!!!';
+// Configuration
+if (file_exists('config.php')) {
+	require_once('config.php');
+}
+
+// Startup
+require_once(DIR_SYSTEM . 'startup.php'); 
+
+
+
+// Registry
+$registry = new Registry();
+
+// Loader
+$loader = new Loader($registry);
+$registry->set('load', $loader);
+
+// Config
+$config = new Config();
+$registry->set('config', $config);
+
+// Database 
+$db = new DB(DB_DRIVER, DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+$registry->set('db', $db);
+
+// Settings
+$query = $db->query("SELECT * FROM " . DB_PREFIX . "setting");
+
+foreach ($query->rows as $setting) {
+	if (!$setting['serialized']) {
+		$config->set($setting['key'], $setting['value']);
+	} else {
+		$config->set($setting['key'], unserialize($setting['value']));
+	}
+}
+
+
+// Url
+$url = new Url($config->get('config_url'), $config->get('config_secure') ? $config->get('config_ssl') : $config->get('config_url'));	
+$registry->set('url', $url);
+
+// Request
+$request = new Request();
+$registry->set('request', $request);
+
+// Response
+$response = new Response();
+$response->addHeader('Content-Type: text/html; charset=utf-8');
+$response->setCompression($config->get('config_compression'));
+$registry->set('response', $response); 
+
+// Cache
+$cache = new Cache();
+$registry->set('cache', $cache);
+
+// Session
+$session = new Session();
+$registry->set('session', $session);
+
+// Document
+$registry->set('document', new Document()); 
+
+// Front Controller 
+$controller = new Front($registry);
+
+// SEO URL's
+$controller->addPreAction(new Action('common/seo_url'));	
+
+// Router
+if (isset($request->get['route'])) {
+	$action = new Action($request->get['route']);
+} else {
+	$action = new Action('common/home');
+}
+
+// Dispatch
+$controller->dispatch($action, new Action('error/not_found'));
+
+// Output
+$response->output();
+
+//echo '<pre>';
+//print_r($registry);
+//echo '</pre>';
 ?>
